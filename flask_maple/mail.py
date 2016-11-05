a50f02c2fe046d17d761333793f15971e164b2f7
honmaple <xiyang0807@gmail.com>
2016-11-05 19:53:00 +0800

    Update
    auth use MethodView
    add response,serializer,view

--- flask_maple/mail.py
+++ flask_maple/mail.py
@@ -7,33 +7,34 @@
 #   Mail:xiyang0807@gmail.com
 #   Created Time: 2015-11-27 21:59:02
 # *************************************************************************
+from flask_mail import Mail
 from flask_mail import Message
 from threading import Thread
 from itsdangerous import URLSafeTimedSerializer
 from flask import current_app
 
 
-class MapleMail(object):
-    def __init__(self, app, mail):
+class MapleMail(Mail):
+    def init_app(self, app):
         self.app = app
-        self.mail = mail
+        super(MapleMail, self).init_app(app)
 
     def send_async_email(self, app, msg):
         with app.app_context():
-            self.mail.send(msg)
+            self.send(msg)
 
-    def email_send(self, to, template, subject):
+    def custom_email_send(self, to, template, subject):
         msg = Message(subject, recipients=[to], html=template)
         thr = Thread(target=self.send_async_email, args=[self.app, msg])
         thr.start()
 
-    def email_token(self, email):
+    def custom_email_token(self, email):
         config = current_app.config
         serializer = URLSafeTimedSerializer(config['SECRET_KEY'])
         token = serializer.dumps(email, salt=config['SECURITY_PASSWORD_SALT'])
         return token
 
-    def confirm_token(self, token, expiration=360):
+    def custom_confirm_token(self, token, expiration=360):
         config = current_app.config
         serializer = URLSafeTimedSerializer(config['SECRET_KEY'])
         try:
